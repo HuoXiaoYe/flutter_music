77116756234b1fa8e4def3b1b57f8263841004a5
huoxiaoye <17711034@bjtu.edu.cn>
2019-09-01 18:51:49 +0800

    轮播图制作完毕

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 import '../api/get_data.dart';
 import '../config/url.dart';
 import '../model/banner.dart';
@@ -24,25 +25,62 @@ class _HomeState extends State<Home> {
   }
 }
 
+// 顶部搜索框
+
+// 轮播图组件
 class Banner extends StatefulWidget {
   @override
   _BannerState createState() => _BannerState();
 }
 
 class _BannerState extends State<Banner> {
+  List bannerList = [];
   @override
   Widget build(BuildContext context) {
-    getBannerList();
+    getBannerList(); // 调用获取轮播图数据的方法
     return Container(
       height: 200,
-      color: Colors.red,
+      color: Colors.white,
+      child: Swiper(
+        itemCount: bannerList.length,
+        itemBuilder: (BuildContext context, int index) {
+          return new Container(
+            padding: EdgeInsets.fromLTRB(15.0, 10.0, 15.0, 0.0),
+            child: ClipRRect(
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+              child: Image.network(
+                bannerList[index].picUrl,
+                fit: BoxFit.fitHeight,
+              ),
+            ),
+            // color: Colors.red,
+            // child: Container(
+            //   height: 190.0,
+            //   decoration: BoxDecoration(
+            //       // borderRadius: BorderRadius.all(Radius.circular(10))
+            //       ),
+            //   child: ClipRRect(
+            //     borderRadius: BorderRadius.all(Radius.circular(10)),
+            //     child: Image.network(
+            //       bannerList[index].picUrl,
+            //       fit: BoxFit.fitHeight,
+            //     ),
+            //   ),
+            // ),
+          );
+        },
+        pagination: new SwiperPagination(),
+      ),
     );
   }
 
   getBannerList() async {
     await request(api["banner"]).then((val) {
       BannerModel list = BannerModel.fromJson(jsonDecode(val));
-      print(list.data.slider[0].picUrl);
+      // print(list.data.slider);
+      setState(() {
+        bannerList = list.data.slider;
+      });
       // List slider = (val as Map)["data"]["slider"];
       // print(slider);
       // print("============");
