42a3e6536e3df7133431c42788be1f6adf67bd39
huoxiaoye <17711034@bjtu.edu.cn>
2019-09-01 16:20:01 +0800

    获取轮播图数据

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import '../api/get_data.dart';
+import '../config/url.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -8,8 +10,35 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("音乐馆"),
+    return Container(
+      height: MediaQuery.of(context).size.height,
+      child: ListView(
+        children: <Widget>[
+          Banner(), // 轮播图
+        ],
+      ),
     );
+  }
+}
+
+class Banner extends StatefulWidget {
+  @override
+  _BannerState createState() => _BannerState();
+}
+
+class _BannerState extends State<Banner> {
+  @override
+  Widget build(BuildContext context) {
+    getBannerList();
+    return Container(
+      height: 200,
+      color: Colors.red,
+    );
+  }
+
+  getBannerList() async {
+    await request(api["banner"]).then((val) {
+     print(val);
+    });
   }
 }
