8d7365fdab8dae90035a70d3f14772adc88a2de0
huoxiaoye <17711034@bjtu.edu.cn>
2019-09-01 19:13:10 +0800

    顶部搜索框 ui 制作完毕

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -18,6 +18,7 @@ class _HomeState extends State<Home> {
       height: MediaQuery.of(context).size.height,
       child: ListView(
         children: <Widget>[
+          SearchContainer(), // 顶部搜索框
           Banner(), // 轮播图
         ],
       ),
@@ -26,6 +27,61 @@ class _HomeState extends State<Home> {
 }
 
 // 顶部搜索框
+class SearchContainer extends StatefulWidget {
+  @override
+  _SearchContainerState createState() => _SearchContainerState();
+}
+
+class _SearchContainerState extends State<SearchContainer> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      height:40,
+      child: Row(
+        children: <Widget>[
+          Container(
+            alignment: Alignment.center,
+            width: 80,
+            child: Text("音乐馆",style: TextStyle(fontSize: 18,fontWeight:FontWeight.w500),),
+          ),
+          Expanded(
+            child:InkWell(
+              onTap: (){
+                // 跳转到 搜索页面
+              },
+              child: Container(
+                alignment: Alignment.center,
+                height: 30,
+                decoration: BoxDecoration(color: Colors.black12,borderRadius: BorderRadius.all(Radius.circular(10))),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(Icons.search,color: Colors.black87),
+                    Padding(
+                      padding: EdgeInsets.only(left: 4),
+                      child: Text("搜索",style: TextStyle(color: Colors.black87),),
+                    )
+                  ],
+                ),
+              ),
+            )
+          ),
+          InkWell(
+            onTap: (){
+              // 跳转到 听歌识曲页面
+            },
+            child: Container(
+              alignment: Alignment.center,
+              width: 80,
+              child: Icon(Icons.music_note,size: 30,color: Colors.black54,),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
 
 // 轮播图组件
 class Banner extends StatefulWidget {
@@ -45,7 +101,7 @@ class _BannerState extends State<Banner> {
         itemCount: bannerList.length,
         itemBuilder: (BuildContext context, int index) {
           return new Container(
-            padding: EdgeInsets.fromLTRB(15.0, 10.0, 15.0, 0.0),
+            padding: EdgeInsets.fromLTRB(15.0, 5.0, 15.0, 0.0),
             child: ClipRRect(
               borderRadius: BorderRadius.all(Radius.circular(10)),
               child: Image.network(
